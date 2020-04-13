4d39a2a0a108ccd52cf8a1dcea7f1ef954be7603
Renann Souza <renann_puera@hotmail.com>
2020-04-13 17:57:55 -0300

    Validate date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -8,10 +9,20 @@ const appointments = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate)
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
   appointments.push(appointment);
   return response.json(appointment);
