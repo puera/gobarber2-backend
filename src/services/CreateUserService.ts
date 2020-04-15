c05c781d4b01e373b7834ebb6a6e796c57328b96
Renann Souza <renann_puera@hotmail.com>
2020-04-15 16:22:03 -0300

    encrypts password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -17,13 +19,17 @@ class CreateUserService {
 
     if (checkUserExists) throw Error('E-mail address already used.');
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
+
+    delete user.password;
 
     return user;
   }
