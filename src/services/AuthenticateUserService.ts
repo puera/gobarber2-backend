6f12d48c6635e4fcf8a9c2e5855775d7bd575aeb
Renann Souza <renann_puera@hotmail.com>
2020-04-16 18:42:58 -0300

    generate jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -7,8 +8,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<User> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -19,7 +25,12 @@ class AuthenticateUserService {
 
     if (!passwordMatched) throw Error('Incorrect e-mail/password combination');
 
-    return user;
+    const token = sign({}, '32b5dd920caea2b09475ed4946167a12', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
