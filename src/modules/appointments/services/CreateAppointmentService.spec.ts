a22dbf2277a5c4e1b6805422fac237878c63593a
Renann Souza <renann_puera@hotmail.com>
2020-05-05 14:06:06 -0300

    Create test CreateAppointment

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '2515151',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('2515151');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '2515151',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '2515151',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
